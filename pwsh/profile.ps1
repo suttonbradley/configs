fc24d3c4c296ee3cc972d3fbe25d24b5e03e4f5a
Sutton Bradley (HE/HIM) <suttonb@microsoft.com>
2024-02-06 15:06:52 -0800

    Move into terminal-specific dirs

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -2,17 +2,17 @@
 # This is a blanket profile that imports/runs other config scripts,
 # but only after pulling the latest version.
 
-# COPY THIS INTO YOUR PROFILE:  . $env:USERPROFILE\code\configs\profile.ps1
+# COPY THIS INTO YOUR PROFILE:  . $env:USERPROFILE\code\configs\pwsh\profile.ps1
 
 # Pull any updates
 Write-Host "Pulling profile updates..."
-git -C $env:USERPROFILE\code\configs pull | Out-Null
+git -C $env:USERPROFILE\code\configs\pwsh pull | Out-Null
 
 # Set up prompt
 Write-Host "Configuring prompt..."
-. $env:USERPROFILE\code\configs\prompt.ps1
+. $env:USERPROFILE\code\configs\pwsh\prompt.ps1
 
 # Import utils
 Write-Host "Importing utils..."
-. $env:USERPROFILE\code\configs\utils.ps1
+. $env:USERPROFILE\code\configs\pwsh\utils.ps1
 # ------------------------------------------
