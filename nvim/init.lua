1cc647428aab369f7a9a139ea2769c5c2e9b7bea
Sutton Bradley <sbradley4646@gmail.com>
2025-03-20 11:43:46 -0700

    lazyvim -> lazy.nvim, whoops

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
--- bootstrap lazy.nvim, LazyVim, and plugins
-require("config.lazy")
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+require("config.lazy")
