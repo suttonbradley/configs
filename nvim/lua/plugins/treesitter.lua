aae57084b65ce9b78ab205bdc7227f106a6a5c8b
Sutton Bradley <sbradley4646@gmail.com>
2025-03-23 14:52:21 -0700

    Treesitter auto-install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,36 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "bash",
-            "git_config",
-            "git_rebase",
-            "html",
-            "javascript",
-            "json",
-            "lua",
-            "markdown_inline",
-            "markdown",
-            "nu",
-            "powershell",
-            "python",
-            "query",
-            "regex",
-            "rust",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "yaml",
-        },
-    },
+    build = ":TSUpdate",
+    config = function () 
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            sync_install = false,
+            highlight = { enable = true }, --TODO remove?
+            indent = { enable = true },  
+            ensure_installed = {
+                "bash",
+                "c",
+                "git_config",
+                "git_rebase",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown_inline",
+                "markdown",
+                "nu",
+                "powershell",
+                "python",
+                "query",
+                "regex",
+                "rust",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "yaml",
+            },
+        })
+    end
 }
