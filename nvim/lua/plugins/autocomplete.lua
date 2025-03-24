b6d2ce0ae2bb3b742851eb9d6b9c92ed50dda380
Sutton Bradley <sbradley4646@gmail.com>
2025-03-24 09:19:53 -0700

    Delete nvim-cmp code that disabled command completion

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -30,39 +30,9 @@ return {{
                 name = 'luasnip'
             }, {
                 name = 'nvim_lsp'
-            }}),
-        })
-
-        -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-        -- Set configuration for specific filetype.
-        --[[ cmp.setup.filetype('gitcommit', {
-                sources = cmp.config.sources({
-                    { name = 'git' },
-                }, {
-                    { name = 'buffer' },
-                })
-            })
-            require("cmp_git").setup() ]] --
-
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({'/', '?'}, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {{
+            }, {
                 name = 'buffer'
-            }}
-        })
-
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({{
-                name = 'path'
-            }}, {{
-                name = 'cmdline'
-            }}),
-            matching = {
-                disallow_symbol_nonprefix_matching = false
-            }
+            }})
         })
 
         -- Set up lspconfig.
