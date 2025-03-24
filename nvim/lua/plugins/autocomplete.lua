024b418c2ca4592980175de3b70aeee19019436f
Sutton Bradley <sbradley4646@gmail.com>
2025-03-24 08:27:56 -0700

    Fix autocomplete, add qq and qqq shortcuts, add git gutters

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -1,23 +1,15 @@
-return {{"hrsh7th/cmp-nvim-lsp"}, {
+return {{
+    "L3MON4D3/LuaSnip",
+    dependencies = {"saadparwaiz1/cmp_luasnip"}
+}, {"hrsh7th/cmp-nvim-lsp"}, {
     "hrsh7th/nvim-cmp",
     config = function()
         local cmp = require 'cmp'
 
         cmp.setup({
             snippet = {
-                -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
-                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-                    -- For `mini.snippets` users:
-                    -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-                    -- insert({ body = args.body }) -- Insert at cursor
-                    -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-                    -- require("cmp.config").set_onetime({ sources = {} })
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
                 end
             },
             window = {
@@ -35,26 +27,22 @@ return {{"hrsh7th/cmp-nvim-lsp"}, {
                 }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             }),
             sources = cmp.config.sources({{
+                name = 'luasnip'
+            }, {
                 name = 'nvim_lsp'
-            } -- { name = 'vsnip' }, -- For vsnip users.
-            -- { name = 'luasnip' }, -- For luasnip users.
-            -- { name = 'ultisnips' }, -- For ultisnips users.
-            -- { name = 'snippy' }, -- For snippy users.
-            }, {{
-                name = 'buffer'
-            }})
+            }}),
         })
 
         -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
         -- Set configuration for specific filetype.
         --[[ cmp.setup.filetype('gitcommit', {
-            sources = cmp.config.sources({
-                { name = 'git' },
-            }, {
-                { name = 'buffer' },
+                sources = cmp.config.sources({
+                    { name = 'git' },
+                }, {
+                    { name = 'buffer' },
+                })
             })
-        })
-        require("cmp_git").setup() ]] -- 
+            require("cmp_git").setup() ]] --
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
         cmp.setup.cmdline({'/', '?'}, {
