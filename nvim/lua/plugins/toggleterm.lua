ed3aa501bea27e73e302df9bf6da8b06525cabdc
Sutton Bradley <sbradley4646@gmail.com>
2025-04-01 10:49:27 -0700

    Add toggle term

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {{
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+        -- More opts at https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#setup
+        open_mapping = [[<leader>tt]],
+        direction = 'float',
+        start_in_insert = true,
+    }
+}}
