0d71dbb15d9a7da1a941706da8c6844ad94ee52c
Sutton Bradley <sutton.bradley@disney.com>
2025-11-18 12:05:16 -0800

    turn off insert_mappings in toggleterm to avoid leader timeout delay when typing space

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,10 +1,11 @@
-return {{
+return { {
     'akinsho/toggleterm.nvim',
     version = "*",
     opts = {
         -- More opts at https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#setup
         open_mapping = [[<leader>tt]],
         direction = 'float',
-        start_in_insert = true
+        start_in_insert = true,
+        insert_mappings = false,
     }
-}}
+} }
