bda87d149591dcf4a49dfc8c84f72893e837823d
Sutton Bradley <sbradley4646@gmail.com>
2026-02-18 10:43:45 -0800

    Update to new treesitter API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,19 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        local configs = require("nvim-treesitter.configs")
+        require('nvim-treesitter').install({
+            "bash", "c", "git_config", "git_rebase", "html", "javascript", "json", "lua",
+            "markdown_inline", "markdown", "nu", "powershell", "python", "query", "regex", "rust",
+            "toml", "tsx", "typescript", "vim", "yaml"
+        })
 
-        configs.setup({
-            sync_install = false,
-            highlight = {
-                enable = true
-            }, -- TODO remove?
-            indent = {
-                enable = true
-            },
-            ensure_installed = {"bash", "c", "git_config", "git_rebase", "html", "javascript", "json", "lua",
-                                "markdown_inline", "markdown", "nu", "powershell", "python", "query", "regex", "rust",
-                                "toml", "tsx", "typescript", "vim", "yaml"}
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.opt_local.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
+            end,
         })
     end
 }
