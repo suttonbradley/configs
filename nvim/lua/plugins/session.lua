f0aabe56bb2448ae12c80f8578671fdb62b60a0b
Sutton Bradley <sutton.bradley@disney.com>
2026-04-15 10:51:58 -0700

    session switcher default to insert

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -8,14 +8,14 @@ return {
     opts = {
         auto_create = false,
         auto_save = true,
-        auto_restore_last_session = true,    -- restore last session if no session for cwd exists
+        auto_restore_last_session = true,   -- restore last session if no session for cwd exists
         auto_delete_empty_sessions = false, -- don't delete sessions just because no named buffers are open
         legacy_cmds = false,                -- removes duplicate "Autosession" (lowercase s) command
-        cwd_change_handling = true, -- save/restore sessions on :cd, keeps rust-toolchain.toml etc. correct
+        cwd_change_handling = true,         -- save/restore sessions on :cd, keeps rust-toolchain.toml etc. correct
         session_lens = {
-            picker_opts = {
-                initial_mode = "normal",
-            },
+            --     picker_opts = {
+            --         initial_mode = "normal",
+            --     },
         },
         post_cwd_changed_cmds = {
             function()
